4f2e43e1a310e9d644765033310f06c076b8aac8
KimTH94 <kcc940602@gmail.com>
2022-06-05 13:48:39 +0900

    [ADD]  API 서버 연결 함수

---
+++ withUs/Model/Response.swift
@@ -0,0 +1,12 @@
+//
+//  Response.swift
+//  withUs
+//
+//  Created by TaeHwan Kim on 6/5/22.
+//
+
+struct Response: Codable {
+    let success: Bool
+    let result: String
+    let message: String
+}
